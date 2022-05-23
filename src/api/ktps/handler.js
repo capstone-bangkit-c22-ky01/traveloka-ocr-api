b8881234cf3b1b01599955dc280cc2011bc9b352
Nurdita Fahira <dhitafahira22@gmail.com>
2022-05-23 10:28:01 +0800

    add imagektp handler

--- src/api/ktps/handler.js
+++ src/api/ktps/handler.js
@@ -28,3 +28,57 @@ async function uploadFile(path, filename) {
     });
     return storage[0].metadata.mediaLink;
 }
+
+// Function for storing the file locally before uploading it to the firebase storage
+async function storeFileUpload(file) {
+    const { filename } = file.hapi;
+    const data = file._data;
+    const ktpFolder = './ktp';
+
+    // create the ktp folder
+    if (!fs.existsSync(ktpFolder)) {
+        fs.mkdirSync(ktpFolder);
+    }
+
+    // ***** MASIH PERLU DIPERBAIKI *****
+    fs.writeFile(`./ktp/${filename}`, data, (err) => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+        console.log("selesai");
+    });
+
+    // call uploadFile function
+    const imagePath = `./ktp/${filename}`;
+    const url = await uploadFile(imagePath, filename);
+    console.log(url);
+    return url;
+}
+
+// handler function
+const addImageKtp = async (request, h) => {
+    const { payload } = request;
+    const id = nanoid(16);
+    const idUser = '';
+    const imageUrl = await storeFileUpload(payload.file);
+
+    //fill database
+    const query = {
+        text: 'INSERT INTO ktps VALUES($1, $2, $3) RETURNING id',
+        values: [id, imageUrl, idUser],
+    };
+
+    const result = await pool.query(query);
+    const imageId = result.rows[0].id;
+
+    const response = h.response({
+        status: 'Succes',
+        message: 'KTP image uccessfully added',
+        data:{
+            imageId,
+        },
+    });
+    response.code(201);
+    return response;
+}
